0cd4804097a5a4037505284e7c6c464e34c365c1
xhaktm <tnwjd318@hs.ac.kr>
2022-05-16 16:12:53 +0900

    제발

--- polls/admin.py
+++ polls/admin.py
@@ -4,21 +4,6 @@ from django.contrib import admin
 from django.contrib import admin
 from polls.models import Question, Choice
 
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 2
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [('Question Statement', {'fields': ['question_text']}),
-                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})]
-
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
 admin.site.register(Choice)
+
--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,13 @@
 from django.db import models
-import datetime
-
 
 # Create your models here.
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
 
-    def was_published_recently(self):
-        return self.pub_date >= datetime.timezone.now() - datetime.timedelta(days=1)
-
     def __str__(self):
         return self.question_text
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
